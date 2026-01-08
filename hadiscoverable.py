bdb85624320257445d6d38373efb26a1d4eca793
dmitry <dimafeofanov@gmail.com>
2026-01-07 16:58:17 -0800

    Update hadiscoverable.py

--- hadiscoverable.py
+++ hadiscoverable.py
@@ -120,7 +120,7 @@ def measure_weight():
 
             sensor_info = SensorInfo(
                 name="wiifitboard",
-                unique_id="wiifitboard",
+                unique_id="00:23:31:75:10:A4",
                 device_class="weight",
                 unit_of_measurement="g",
             )
